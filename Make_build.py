06b7d553aa1c5c904d5781f648533d5519e7db14
BumotecS191 <bumotec@bk.ru>
2022-09-05 15:53:51 +0700

    Initional commit

---
+++ Make_build.py
@@ -0,0 +1,24 @@
+def Make_build(base_block, size_floor, material, amount_floors):
+    text_function = []
+    i = 1
+    while i <= int(amount_floors):
+        # Make floor with capasity
+        stroka = ''
+        stroka = 'fill ' + str(base_block['X']) + ' ' + str(base_block['Y']) + ' ' + str(base_block['Z']) + ' '
+        stroka = stroka + str(base_block['X'] + size_floor['Length_floor']) + ' '
+        stroka = stroka + str(base_block['Y'] + size_floor['Width_floor']) + ' '
+        stroka = stroka + str(base_block['Z'] + size_floor['Hight_floor']) + ' '
+        stroka = stroka + material + '\n'
+        text_function.append(stroka)
+        # Make floor with empty space inside
+        stroka2 = ''
+        stroka2 = 'fill ' + str(base_block['X'] + 1) + ' ' + str(base_block['Y'] + 1) + ' ' + str(base_block['Z'] + 1) + ' '
+        stroka2 = stroka2 + str(base_block['X'] + size_floor['Length_floor'] - 1) + ' '
+        stroka2 = stroka2 + str(base_block['Y'] + size_floor['Width_floor'] - 1) + ' '
+        stroka2 = stroka2 + str(base_block['Z'] + size_floor['Hight_floor'] - 1) + ' '
+        stroka2 = stroka2 + 'air\n'
+        text_function.append(stroka2)
+        #Change Z level of floor
+        base_block['Z'] = base_block['Z'] + size_floor['Hight_floor']
+        i += 1
+    return text_function
---
+++ Minecraft_buildings.py
@@ -0,0 +1,44 @@
+import Make_build
+
+base_block = {'X':0, 'Y':0, 'Z':0}
+size_floor = {'Length_floor':0, 'Width_floor':0, 'Hight_floor':0}
+material = 'air'
+amount_floors = 0
+text_function = []
+
+base_block['X'] = int(input('Enter X = '))
+base_block['Y'] = int(input('Enter Y = '))
+base_block['Z'] = int(input('Enter Z = '))
+
+size_floor['Length_floor'] = int(input('Enter Length of floor = '))
+size_floor['Width_floor'] = int(input('Enter Width of floor = '))
+size_floor['Hight_floor'] = int(input('Enter Length of floor = '))
+
+material = input('Material of floor = ')
+amount_floors = int(input('Amount of the floors = '))
+
+text_function = Make_build.Make_build(base_block, size_floor, material, amount_floors)
+
+for i in text_function:
+    print(i)
+
+
+# Make file with functions
+name_of_file = 'Roma_build.mcfunction'
+way_file = 'c:\\Users\\cnc\\PycharmProjects\\pythonProject\\Mine_craft_fun\\'
+
+write_file = 0
+if write_file == 1:
+    nefile = open(way_file + '\\' + name_of_file, 'w')
+    # Add double cpase for the first Block
+    i = 0
+    while i < len(text_function):
+        nefile.write(text_function[i])
+        i += 1
+    nefile.close()
+
+
+
+
+
+
